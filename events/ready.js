465bd634f9f8c4acf0491d81473172a6b2fa9455
Tjird <tjeerd@tjird.nl>
2019-10-16 07:47:30 +0200

    prod

--- events/ready.js
+++ events/ready.js
@@ -2,12 +2,30 @@ const rediss = require("redis");
 const { redis } = require("../config");
 const sub = rediss.createClient(redis);
 const Discord = require("discord.js");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 sub.on("error", (err) => {
     console.log(`Error ${err}`);
 });
 
 module.exports = (client) => {
+    const app = express();
+
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+    app.listen(3000, () => {
+        console.log("Server is listening on port 3000");
+    });
+
+    app.post('/votes', (req, res) => {
+        console.log(req)
+    });
+
+    app.use((req, res) => {
+        res.status(404).send({ url: req.originalUrl + ' not found' });
+    });
+
     client.user.setActivity(`startup process, give me a moment plz ,_,`, { type: 'PLAYING' });
 
     let usercount = 0;
@@ -43,7 +61,6 @@ module.exports = (client) => {
         notifyChannel.send(embed);
     });
 
-    sub.subscribe("updateStatus");
     sub.subscribe("addedGuild");
     sub.subscribe("leftGuild");
 
