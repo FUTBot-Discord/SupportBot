56e31f83e6fe363b9c4a2252f16e178df062f1d6
Tjird <tjeerd@tjird.nl>
2019-12-01 22:50:46 +0100

    Votes reward added for packbot & join_leave packbot added

--- events/ready.js
+++ events/ready.js
@@ -2,69 +2,150 @@ const rediss = require("redis");
 const { redis } = require("../config");
 const sub = rediss.createClient(redis);
 const Discord = require("discord.js");
-const express = require('express');
-const bodyParser = require('body-parser');
-
-sub.on("error", (err) => {
-    console.log(`Error ${err}`);
+const express = require("express");
+const bodyParser = require("body-parser");
+const { GraphQLClient } = require("graphql-request");
+const graphql = new GraphQLClient(process.env.G_ENDPOINT, {
+  headers: {}
 });
 
-module.exports = (client) => {
-    const app = express();
+async function getUserClubId(author_id) {
+  let query = `{ getUserClubByAuthorId(author_id: "${author_id}") { id points coins creation_time } }`;
+  let res = await graphql.request(query);
 
-    app.use(bodyParser.urlencoded({ extended: true }))
-    app.use(bodyParser.json())
-    app.listen(3000, () => {
-        console.log("Server is listening on port 3000");
-    });
+  return res.getUserClubByAuthorId;
+}
 
-    app.post('/votes', (req, res) => {
-        console.log(req.body);
-        let vGuild = client.guilds.get("470582456828035073");
-        let vChannel = vGuild.channels.get("542756397230718976");
-        vChannel.send(`\`\`\`${JSON.stringify(req.body)}\`\`\``);
-    });
+async function createUserClub(author_id) {
+  let query = `mutation { createUserClub(author_id: "${author_id}") { id } }`;
 
-    app.use((req, res) => {
-        res.status(404).send({ url: req.originalUrl + ' not found' });
-    });
+  try {
+    await graphql.request(query);
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
 
-    client.user.setActivity(`startup process, give me a moment plz ,_,`, { type: 'PLAYING' });
+  return true;
+}
 
-    let usercount = 0;
+async function addPointsToClub(club_id, points) {
+  let query = `mutation { addPointsToClub(club_id: "${club_id}", points: "${points}") { id } }`;
 
-    for (i = 0; i < client.guilds.size; i++) {
-        usercount += client.guilds.array()[i].memberCount;
+  try {
+    await graphql.request(query);
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+
+  return true;
+}
+
+sub.on("error", err => {
+  console.log(`Error ${err}`);
+});
+
+module.exports = client => {
+  const app = express();
+  const guild = client.guilds.get("470582456828035073");
+
+  app.use(
+    bodyParser.urlencoded({
+      extended: true
+    })
+  );
+  app.use(bodyParser.json());
+  app.listen(3000, () => {
+    console.log("Votes can be send to port 3000");
+  });
+
+  app.post("/votes", async (req, res) => {
+    const { bot, user } = req.body;
+    let notifyChannel;
+
+    if (!user || user == undefined) return;
+
+    let iUser = await getUserClubId(user);
+
+    if (iUser == null) {
+      await createUserClub(user);
+      iUser = await getUserClubId(user);
     }
 
-    console.log(`Logged in as ${client.user.tag} and looking at ${usercount} users(${client.guilds.size} guilds).`);
-    client.user.setActivity(`${usercount} users`, { type: 'WATCHING' });
+    addPointsToClub(iUser.id, 75);
 
-    console.log("====================");
+    if (bot === "520694612080328709") {
+      notifyChannel = guild.channels.get("650800677215404083");
+    } else {
+      notifyChannel = guild.channels.get("650800555542708234");
+    }
 
-    sub.on("message", (channel, message) => {
-        let guild = client.guilds.get("470582456828035073");
-        let notifyChannel = guild.channels.get("618398163837124609");
-        let { guildName, guildOwner } = JSON.parse(message);
+    const vUser = await client.fetchUser(user);
+    const embed = new Discord.RichEmbed()
+      .setColor(0x2ff37a)
+      .setTitle(`New vote got recieved`)
+      .setDescription(`${vUser.username} voted for me!`)
+      .setFooter(`Thanks ${guildOwner} for the vote!`);
 
-        if (channel == 'addedGuild') {
-            embed = new Discord.RichEmbed()
-                .setColor(0x2FF37A)
-                .setTitle(`New guild has added me`)
-                .setDescription(`${guildName} can now use commands I serve!`)
-                .setFooter(`Thanks ${guildOwner} for adding me!`);
-        } else if (channel == 'leftGuild') {
-            embed = new Discord.RichEmbed()
-                .setColor(0x2FF37A)
-                .setTitle(`Guild kicked me :(`)
-                .setDescription(`${guildName} is not needing me anymore...`)
-                .setFooter(`I don't love you ${guildOwner}`);
-        }
+    notifyChannel.send(embed);
+  });
 
-        notifyChannel.send(embed);
+  app.use((req, res) => {
+    res.status(404).send({
+      url: req.originalUrl + " not found"
     });
+  });
 
-    sub.subscribe("addedGuild");
-    sub.subscribe("leftGuild");
+  client.user.setActivity(`startup process, give me a moment plz ,_,`, {
+    type: "PLAYING"
+  });
 
+  let usercount = 0;
+
+  for (i = 0; i < client.guilds.size; i++) {
+    usercount += client.guilds.array()[i].memberCount;
+  }
+
+  console.log(
+    `Logged in as ${client.user.tag} and looking at ${usercount} users(${client.guilds.size} guilds).`
+  );
+
+  client.user.setActivity(`${usercount} users`, {
+    type: "WATCHING"
+  });
+
+  console.log("====================");
+
+  sub.on("message", (channel, message) => {
+    const { guildName, guildOwner, botId } = JSON.parse(message);
+    const embed = new Discord.RichEmbed();
+
+    let notifyChannel;
+
+    if (channel == "addedGuild") {
+      embed
+        .setColor(0x2ff37a)
+        .setTitle(`New guild has added me`)
+        .setDescription(`${guildName} can now use commands I serve!`)
+        .setFooter(`Thanks ${guildOwner} for adding me!`);
+    } else {
+      embed
+        .setColor(0xff0000)
+        .setTitle(`Guild kicked me :(`)
+        .setDescription(`${guildName} is not needing me anymore...`)
+        .setFooter(`I don't love you ${guildOwner}`);
+    }
+
+    if (botId === "520694612080328709") {
+      notifyChannel = guild.channels.get("618398163837124609");
+    } else {
+      notifyChannel = guild.channels.get("640560405101543424");
+    }
+
+    notifyChannel.send(embed);
+  });
+
+  sub.subscribe("addedGuild");
+  sub.subscribe("leftGuild");
 };
